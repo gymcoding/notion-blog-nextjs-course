d619556663d365319dcda8cde44187e62de59a99
gymcoding <bruce.lean17@gmail.com>
2025-02-13 10:23:50 +0900

    1 nextjs 설치

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,9 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <p>
+          Hello World!!!!
+        </p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
