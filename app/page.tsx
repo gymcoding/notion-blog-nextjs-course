d372bee1b3dcdf28ce6792e0cf504ac62e9aa653
gymcoding <bruce.lean17@gmail.com>
2025-03-12 15:39:20 +0900

    35.1 react-query-devtools 설치

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { Suspense } from 'react';
 import TagSectionClient from '@/app/_components/TagSection.client';
 import PostListSkeleton from '@/components/features/blog/PostListSkeleton';
 import TagSectionSkeleton from '@/app/_components/TagSectionSkeleton';
-import PostList from '@/components/features/blog/PostList';
 interface HomeProps {
   searchParams: Promise<{ tag?: string; sort?: string }>;
 }
@@ -18,7 +17,7 @@ export default async function Home({ searchParams }: HomeProps) {
   const selectedSort = sort || 'latest';
 
   const tags = getTags();
-  const postsPromise = await getPublishedPosts({ tag: selectedTag, sort: selectedSort });
+  const postsPromise = getPublishedPosts({ tag: selectedTag, sort: selectedSort });
   return (
     <div className="container py-8">
       <div className="grid grid-cols-[200px_1fr_220px] gap-6">
@@ -32,10 +31,9 @@ export default async function Home({ searchParams }: HomeProps) {
           {/* 섹션 제목 */}
           <HeaderSection selectedTag={selectedTag} />
           {/* 블로그 카드 그리드 */}
-          {/* <Suspense fallback={<PostListSkeleton />}>
+          <Suspense fallback={<PostListSkeleton />}>
             <PostListSuspense postsPromise={postsPromise} />
-          </Suspense> */}
-          <PostList posts={postsPromise.posts} />
+          </Suspense>
         </div>
         {/* 우측 사이드바 */}
         <aside className="flex flex-col gap-6">
