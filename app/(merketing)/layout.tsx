f19f16987a79d340e5c29d57623ab4ff5225daf7
gymcoding <bruce.lean17@gmail.com>
2025-04-02 08:55:02 +0900

    47.2 route group root layout

--- app/(merketing)/layout.tsx
+++ app/(merketing)/layout.tsx
@@ -1,8 +1,12 @@
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <div>Marketing Page</div>
-      <main className="flex-1">{children}</main>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="flex min-h-screen flex-col">
+          <div>Marketing Page</div>
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
+    </html>
   );
 }
