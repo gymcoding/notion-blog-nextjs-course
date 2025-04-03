ec881816285542287136cc0ee987b2b6cbaa5b35
gymcoding <bruce.lean17@gmail.com>
2025-04-03 14:21:57 +0900

    49.2 Loading and Error UI

--- app/dashboard/@stats/test/page.tsx
+++ app/dashboard/@stats/test/page.tsx
@@ -1,3 +1,6 @@
-export default function TestPage() {
-  return <div>Stats TestPage</div>;
+export default async function TestPage() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  throw new Error('Test error');
+  // return <div>Stats TestPage</div>;
 }
